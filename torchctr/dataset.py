755171492445efedbb08dbf85a1471821f364795
xiahouzuoxin <xiahouzuoxin@163.com>
2024-11-12 11:58:58 +0800

    add get_dataloader_from_parquets

--- torchctr/dataset.py
+++ torchctr/dataset.py
@@ -4,6 +4,8 @@ import polars as pl
 import torch
 import torch.utils
 from torch.utils.data import Dataset
+from datasets import load_dataset
+from .utils import pad_sequences_to_maxlen
 from .utils import pad_list, logger
 from .transformer import FeatureTransformer
 
@@ -252,3 +254,73 @@ def get_dataloader(ds: DataFrameDataset, **kwargs):
     Get DataLoader from DataFrameDataset.
     '''
     return torch.utils.data.DataLoader(ds, collate_fn=ds.collate_fn, **kwargs)
+
+def get_dataloader_from_parquets(parquets, 
+                                 feat_configs, 
+                                 target_cols, 
+                                 list_padding_value=-100, 
+                                 list_padding_maxlen=256,
+                                 **kwargs):
+    '''
+    Get DataLoader from parquet files.
+
+    Args:
+        parquets: list of str, parquet files (have transformed by FeatureTransformer)
+        feat_configs: list of dict, feature configurations for FeatureTransformer
+        target_cols: list of str, target columns
+        list_padding_value: int, padding value for list features, not used if no list features
+        list_padding_maxlen: int, maximum length for padding list features, not used if no list features
+        kwargs: DataLoader parameters
+
+    Returns:
+        DataLoader object. The batch data format is tuple(features, labels).
+        (features, labels) = (
+            {
+                'dense_features': tensor, 
+                'sparse_feature1': tensor, 
+                'sparse_feature2': tensor, 
+                'seq_sparse_feature1': tensor,
+                ...
+            },     # features
+            tensor # labels
+        )
+    '''
+
+    ds = load_dataset('parquet', data_files=parquets, columns=[f['name'] for f in feat_configs] + target_cols)
+    ds = ds.with_format('torch')
+
+    def collate_fn(batch):
+        # list of dict to dict of list
+        # print(batch)
+
+        batch_dense = []
+        batch_sparse = {}
+        for k in feat_configs:
+            if k['type'] == 'dense':
+                # print(k['name'])
+                batch_dense.append(
+                    torch.tensor([sample[k['name']] for sample in batch], dtype=torch.float32)
+                )
+            elif k['type'] == 'sparse':
+                if k.get('islist'):
+                    sparse_feat = [sample[k['name']] for sample in batch]
+                    # pad sequences
+                    sparse_feat = pad_sequences_to_maxlen(
+                        sparse_feat, batch_first=True, 
+                        padding_value=list_padding_value, 
+                        max_length=list_padding_maxlen)
+                else:
+                    sparse_feat = torch.tensor([[sample[k['name']]] for sample in batch], dtype=torch.long)
+                batch_sparse[k['name']] = sparse_feat
+
+        batch_features = {
+            'dense_features': torch.stack(batch_dense, dim=1),
+            **batch_sparse
+        }
+        batch_labels = torch.tensor([[sample[f] for f in target_cols] for sample in batch], dtype=torch.float32)
+        
+        return batch_features, batch_labels
+
+    dl = torch.utils.data.DataLoader(ds, collate_fn=collate_fn, **kwargs)
+
+    return dl
